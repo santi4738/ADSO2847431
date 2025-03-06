435c5bbc48cf6690ceded64ba97212954bd9ceb7
unknown <SENA@MANAPRCPIFSD001.FormacionSena.RED>
2025-03-06 11:59:52 -0500

    new changes

--- 03-laravel/petsapp/database/migrations/0001_01_01_000000_create_users_table.php
+++ 03-laravel/petsapp/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->bigInteger('document')->unique();
+            $table->string('fullname');
+            $table->string('gender');
+            $table->date('birthdate');
+            $table->string('photo')->default('no-photo.png');
+            $table->string('phone');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->default('customer');
             $table->rememberToken();
             $table->timestamps();
         });
